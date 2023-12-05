9678bb71b87ba3ae82d3f576a3093411b8642378
Joanerson Costa <joanersonc31@gmail.com>
2023-12-05 14:02:58 -0300

    inicia projeto

---
+++ src/test/java/dev/wakandaacademy/clinica/ClinicaMedicaApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.wakandaacademy.clinica;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClinicaMedicaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
