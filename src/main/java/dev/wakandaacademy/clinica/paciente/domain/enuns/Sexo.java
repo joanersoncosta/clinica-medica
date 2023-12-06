13b2802a5e3ad9b607082afd21599caa684cc7b2
Joanerson Costa <joanersonc31@gmail.com>
2023-12-06 11:30:18 -0300

    impelemnta metodo verifica valor no enem sexo

--- src/main/java/dev/wakandaacademy/clinica/paciente/domain/enuns/Sexo.java
+++ src/main/java/dev/wakandaacademy/clinica/paciente/domain/enuns/Sexo.java
@@ -2,15 +2,24 @@ package dev.wakandaacademy.clinica.paciente.domain.enuns;
 
 public enum Sexo {
 	FEMININO("FEMININO"), MASCULINO("MASCULINO");
-	
+
 	private String sexo;
-	
-	Sexo(String  sexo){
+
+	Sexo(String sexo) {
 		this.sexo = sexo;
 	}
-	
+
 	public String getSexo() {
 		return this.sexo;
 	}
-	
+
+	public static Sexo verificaValor(String sexo) {
+		for (Sexo valorCorrespondente : Sexo.values()) {
+			if (valorCorrespondente.getSexo() == sexo) {
+				return valorCorrespondente;
+			}
+		}
+		throw new IllegalArgumentException("Valor invalido");
+	}
+
 }
