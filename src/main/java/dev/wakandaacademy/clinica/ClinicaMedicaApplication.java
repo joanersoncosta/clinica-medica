966b9ec8b20677b6b8853f78e89e32c977b16af5
Joanerson Costa <joanersonc31@gmail.com>
2023-12-06 07:55:11 -0300

    implementa api de teste

--- src/main/java/dev/wakandaacademy/clinica/ClinicaMedicaApplication.java
+++ src/main/java/dev/wakandaacademy/clinica/ClinicaMedicaApplication.java
@@ -2,12 +2,21 @@ package dev.wakandaacademy.clinica;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
+@RequestMapping("/")
 @SpringBootApplication
 public class ClinicaMedicaApplication {
 
+	@GetMapping
+	public String getHome() {
+		return "Clinica Medica - API Home";
+	}
+	
 	public static void main(String[] args) {
 		SpringApplication.run(ClinicaMedicaApplication.class, args);
 	}
-
 }
